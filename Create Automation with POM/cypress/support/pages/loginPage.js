e9eb8e1d07e009ccd9ba7fecdde50a189d38f117
thaniaputri <thaniaputri.gunadarma@gmail.com>
2026-05-08 22:23:06 +0700

    add Cypress POM OrangeHRM project

---
+++ Create Automation with POM/cypress/support/pages/loginPage.js
@@ -0,0 +1,47 @@
+class loginPage {
+
+    visitPage() {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    }
+
+    usernameField() {
+        return cy.get("input[name='username']")
+    }
+
+    passwordField() {
+        return cy.get("input[name='password']")
+    }
+
+    loginButton() {
+        return cy.get("button[type='submit']")
+    }
+
+    inputUsername(username) {
+        this.usernameField().clear().type(username)
+    }
+
+    inputPassword(password) {
+        this.passwordField().clear().type(password)
+    }
+
+    clickLogin() {
+        this.loginButton().click()
+    }
+
+    verifyDashboard() {
+        cy.url().should('include', '/dashboard')
+    }
+
+    verifyInvalidCredentials() {
+        cy.contains('Invalid credentials', { timeout: 10000 })
+            .should('be.visible')
+    }
+
+    verifyRequiredMessage() {
+        cy.contains('Required', { timeout: 10000 })
+            .should('be.visible')
+    }
+
+}
+
+export default loginPage
